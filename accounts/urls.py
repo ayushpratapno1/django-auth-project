1eff0332753e79d4d7e89c5cd992a2c5e012554f
Ayush Pratap Singh <ayushpratapno1@gmail.com>
2026-05-30 00:03:48 +0530

    Login Feature added

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import home, signup
+from .views import home, signup, user_login
 
 urlpatterns = [
     path('', home, name='home'),
     path('signup/', signup, name='signup'),
+    path('login/', user_login, name='login'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 from django.shortcuts import render, redirect
 from .forms import SignUpForm
 
@@ -21,3 +23,31 @@ def signup(request):
         form = SignUpForm()
 
     return render(request, 'signup.html', {'form': form})
+
+def user_login(request):
+
+    if request.method == 'POST':
+
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(
+            request,
+            username=username,
+            password=password
+        )
+
+        if user is not None:
+
+            login(request, user)
+
+            return redirect('home')
+
+        else:
+
+            messages.error(
+                request,
+                'Invalid username or password'
+            )
+
+    return render(request, 'login.html')
