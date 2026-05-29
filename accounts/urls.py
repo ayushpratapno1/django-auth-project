b12e0665a7f99ac110eac706b9600f52cd49ad2a
Ayush Pratap Singh <ayushpratapno1@gmail.com>
2026-05-29 23:47:59 +0530

    Signup feature added

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import home, signup
 
 urlpatterns = [
     path('', home, name='home'),
+    path('signup/', signup, name='signup'),
 ]
