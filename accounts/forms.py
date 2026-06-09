bbb60a2afc675f1039d8636b3512e315a19545f1
Ayush Pratap Singh <ayushpratapno1@gmail.com>
2026-06-09 14:17:28 +0530

    Upgrade with UI improvements

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,10 +5,12 @@ from django.contrib.auth.forms import UserCreationForm
 
 
 class SignUpForm(UserCreationForm):
+
     email = forms.EmailField(required=True)
 
     class Meta:
         model = User
+
         fields = [
             'username',
             'first_name',
@@ -17,6 +19,27 @@ class SignUpForm(UserCreationForm):
             'password1',
             'password2'
         ]
+
+    def __init__(self, *args, **kwargs):
+
+        super().__init__(*args, **kwargs)
+
+        placeholders = {
+            'username': 'Enter username',
+            'first_name': 'Enter first name',
+            'last_name': 'Enter last name',
+            'email': 'Enter email',
+            'password1': 'Create password',
+            'password2': 'Confirm password'
+        }
+
+        for field_name, field in self.fields.items():
+
+            field.widget.attrs.update({
+                'class': 'form-control auth-input',
+                'placeholder': placeholders.get(field_name, '')
+            })
+
 
 class ProfileForm(forms.ModelForm):
 
@@ -29,3 +52,25 @@ class ProfileForm(forms.ModelForm):
             'github',
             'linkedin'
         ]
+
+    def __init__(self, *args, **kwargs):
+
+        super().__init__(*args, **kwargs)
+
+        placeholders = {
+            'bio': 'Tell us about yourself...',
+            'github': 'https://github.com/username',
+            'linkedin': 'https://linkedin.com/in/username'
+        }
+
+        for field_name, field in self.fields.items():
+
+            field.widget.attrs.update({
+                'class': 'form-control'
+            })
+
+            if field_name in placeholders:
+
+                field.widget.attrs.update({
+                    'placeholder': placeholders[field_name]
+                })
